5208670de63416c7487701e02ecbab3552120e3b
Sam Bauch <sbauch@gmail.com>
2020-06-11 10:11:36 -0400

    rm pry

--- lib/xbox_live_api/requests/presence_request.rb
+++ lib/xbox_live_api/requests/presence_request.rb
@@ -19,31 +19,15 @@ class XboxLiveApi
         params = {
             'users' => user_ids
         }
-        print params
+        
         HttpSessionGateway.new.post_json(url, header: header_for_version(Version::NEXT).merge('Host': 'userpresence.xboxlive.com'), body: params).body
       end
 
       def handle_response(resp, user_id)
-        binding.pry
         json = Oj.load(resp)
-        settings = json['profileUsers'].first['settings']
-        settings_hash = collect_settings(settings)
-        Profile.new(id: user_id,
-                    gamertag: settings_hash['Gamertag'],
-                    gamerscore: settings_hash['Gamerscore'].to_i,
-                    gamer_picture: settings_hash['GameDisplayPicRaw'],
-                    account_tier: settings_hash['AccountTier'],
-                    xbox_one_rep: settings_hash['XboxOneRep'],
-                    preferred_color_url: settings_hash['PreferredColor'],
-                    tenure_level: settings_hash['TenureLevel'].to_i)
-      end
+        # TODO: PresenceRecord, DeviceRecord classes
 
-      def collect_settings(settings)
-        settings_hash = {}
-        settings.each do |setting|
-          settings_hash.store(setting['id'], setting['value'])
-        end
-        settings_hash
+        json
       end
     end
   end
